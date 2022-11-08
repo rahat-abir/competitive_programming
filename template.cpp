3749c086f12af9c47e6bf3aa62449bda65526b5c
rahat-abir <72164850+rahat-abir@users.noreply.github.com>
2022-11-09 01:37:50 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pob pop_back
+typedef vector<int> vi;
+#define nl cout<<"\n";
+#define for0(i, j, k, in) for (int i = j; i < k; i += in)
+#define out(n) cout<<n<<endl;
+
+void pm(map<char , bool> m){
+    map<char , bool> :: iterator it;
+    for (it =m.begin(); it != m.end(); ++it) {
+        cout <<it->first <<' '<< it->second<<endl;
+    }
+
+}
+
+void pv(vector<int> v){
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<spc;
+    }nl;
+}
+
+
+
+
+
+void solve(){
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+int main()
+{
+//    #ifndef ONLINE JUDGE
+//    freopen("test_input.txt","r",stdin);
+//    #endif
+    ios::sync_with_stdio(0);  cin.tie(0);
+    num();
+    //int n; cin>>n;
+    //while(n--)
+        solve();
+
+
+    return 0;
+}
+
+
