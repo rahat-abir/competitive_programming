acf1280d51865d329d625922133b30543ccbd3e7
rahat-abir <72164850+rahat-abir@users.noreply.github.com>
2022-11-26 01:50:02 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -8,20 +8,7 @@ typedef vector<int> vi;
 #define nl cout<<"\n";
 #define for0(i, j, k, in) for (int i = j; i < k; i += in)
 #define out(n) cout<<n<<endl;
-
-void pm(map<char , bool> m){
-    map<char , bool> :: iterator it;
-    for (it =m.begin(); it != m.end(); ++it) {
-        cout <<it->first <<' '<< it->second<<endl;
-    }
-
-}
-
-void pv(vector<int> v){
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<spc;
-    }nl;
-}
+string str=" ";
 
 
 
@@ -51,7 +38,6 @@ int main()
 //    freopen("test_input.txt","r",stdin);
 //    #endif
     ios::sync_with_stdio(0);  cin.tie(0);
-    num();
     //int n; cin>>n;
     //while(n--)
         solve();
