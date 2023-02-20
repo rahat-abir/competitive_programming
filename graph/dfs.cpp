3517203ba3172186ac2aca5d27897c3681942b69
rahat-abir <72164850+rahat-abir@users.noreply.github.com>
2023-02-21 04:04:29 +0600

    Create dfs.cpp

---
+++ graph/dfs.cpp
@@ -0,0 +1,13 @@
+const int N= 1e5+10;
+vector<int> g[N];
+bool vis[N];
+int cnt=0;
+void dfs(int vertex){//after entering the vertex
+	vis[vertex] = true;
+	for(int child : g[vertex]){
+		//after entering child
+		if(vis[child]) continue;
+		dfs(child);
+//before exitinng the child
+	}
+}
